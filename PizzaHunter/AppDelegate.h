865b81767fbc22ef20685598aaa4dabb4ddd3fee
xeaza <taylorwrightsanson@gmail.com>
2014-10-15 14:36:47 -0500

    Initial Commit

---
+++ PizzaHunter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PizzaHunter
+//
+//  Created by Taylor Wright-Sanson on 10/15/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
