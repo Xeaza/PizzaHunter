a977fdaa3b7baab405c0ee5d7a8de37e789d502d
xeaza <taylorwrightsanson@gmail.com>
2014-10-16 09:27:54 -0500

    Bit more progress on step 6

--- PizzaHunter/MapViewController.m
+++ PizzaHunter/MapViewController.m
@@ -33,7 +33,7 @@
         [self.mapView addAnnotation:pizzeria.placemark];
         self.pizzeria = pizzeria;
         // Need to make detail discloser open a segue to a new mapview that loads my locaiton and finds the route to the pizza place i clicked on.
-        [self findRoute];
+        //[self findRoute];
     }
 }
 
@@ -63,6 +63,30 @@
     return routeLineRenderer;
 }
 
+- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control
+{
+    //[self performSegueWithIdentifier:@"AnnotationSegue" sender:view];
+    [self findRoute];
+  //  accessoryButtonTappedForRowWithIndexPath
+}
+
+- (void)mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)view
+{
+    NSLog(@"HI");
+    // TODO - When user taps on specific annotation set the self.pizzeria equal to the pizzeria associated with that annotation
+    // then call [self findRoute]; so user can get a route from their location to the pizza place they tapped
+//    for (Pizzeria *pizzeria in pizzeriaListViewController.pizzerias)
+//    {
+//        if (pizzeria.location == view.annotation.coordinate) {
+//
+//        }
+//        [self.mapView addAnnotation:pizzeria.placemark];
+//        self.pizzeria = pizzeria;
+        // Need to make detail discloser open a segue to a new mapview that loads my locaiton and finds the route to the pizza place i clicked on.
+        //[self findRoute];
+//    }
+}
+
 - (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
 {
     if (annotation == mapView.userLocation)
@@ -78,20 +102,20 @@
     return pin;
 }
 
--(void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control
-{
-    CLLocationCoordinate2D center = view.annotation.coordinate;
-
-    MKCoordinateSpan span;
-    span.latitudeDelta = 0.01;
-    span.longitudeDelta = 0.01;
-
-    MKCoordinateRegion region;
-    region.center = center;
-    region.span = span;
-
-    [self.mapView setRegion:region animated:YES];
-}
+//-(void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control
+//{
+//    CLLocationCoordinate2D center = view.annotation.coordinate;
+//
+//    MKCoordinateSpan span;
+//    span.latitudeDelta = 0.01;
+//    span.longitudeDelta = 0.01;
+//
+//    MKCoordinateRegion region;
+//    region.center = center;
+//    region.span = span;
+//
+//    [self.mapView setRegion:region animated:YES];
+//}
 
 - (void)mapViewDidFinishLoadingMap:(MKMapView *)mapView
 {
